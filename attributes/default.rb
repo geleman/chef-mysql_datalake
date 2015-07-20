ddffab5d8afc08a8a4def0bb4252d46a56462f12
Dann S Washko <dwashko@gmti.gannett.com>
2015-07-20 15:42:21 -0400

    GDPHAR-60 adding base datalake configuration stuff

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,3 +16,5 @@ node.normal['mysql_config']['innodb_log_buffer_size'] = '48M'
 node.normal['mysql_config']['innodb_flush_log_at_trx_commit'] = '2'
 node.normal['mysql_config']['innodb_stats_on_metadata'] = 'OFF'
 node.normal['mysql_config']['databag_name'] = 'test'
+
+node.normal['mysql_config']['databag_name'] = 'test'
